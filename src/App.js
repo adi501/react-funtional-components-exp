376445d7bccde70fbff1043b680c0a090555fb8b
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-11-21 17:43:14 +0530

    getElementById with style apply using useEffect

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 //import List_using_key from './Components/List_using_key';
 //import Infinite_loop_in_useEffect from './Components/Infinite_loop_in_useEffect';
 //import Error_Boundaries_Exp from './Components/Error_Boundaries_Exp';
-import Axios_example from './Components/Axios_example';
+//import Axios_example from './Components/Axios_example';
+import Get_an_Element_by_ID_in_ReactJS from './Components/Get_an_Element_by_ID_in_ReactJS';
 
 function App() {
   return (
     <div >
-      <Axios_example/>
+      <Get_an_Element_by_ID_in_ReactJS/>
     </div>
   );
 }
---
+++ src/Components/Get_an_Element_by_ID_in_ReactJS.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+ export default function Get_an_Element_by_ID_in_ReactJS() {
+    useEffect(() => {
+        const element = document.getElementById('h1');
+            // Manipulate the retrieved element's style
+            element.style.backgroundColor = 'red';
+            element.style.color = 'white';
+            // Add more styling properties as needed
+    }, [])
+        return (
+            <>
+                <h1 id="h1">
+                    AdiNarayana Reddy
+                </h1>
+            </>
+        );
+}
+
